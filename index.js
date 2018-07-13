1eb32a0729140f67323939f8d992c56ab3df9f94
Christina Tedesco <christdsco@gmail.com>
2018-07-13 17:38:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,15 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+ { console.log(string.toLowerCase())
 }
+
+
 function sayHiToGrandma(string) {
-  return string.toLowerCase()
-}
-function sayHiToGrandma(string) {
-  return string.toUpperCase()
-}
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
